cc1e70252fb8dd0c7d7d42a22d8c7cc1f394cb88
Andresito126 <creed126ax@gmail.com>
2024-06-23 14:48:59 -0600

    se agrego el login

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
-import Home from './Paginas/Home.jsx'
+import Login from './Paginas/LoginPagina.jsx'
 
 function App() {
   return (
     <>
-      <Home/>
+      <Login/>
+
+      
     </>
   );
 }
---
+++ src/Paginas/LoginPagina.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Login from "../Componentes/Login/Login";
+import NavBar from "../Componentes/NavBar/NavBar";
+
+export default function LoginPagina(){
+    return(
+        <>
+        <NavBar/>
+        <Login/>
+        </>
+    );
+}
