0e0378c8f8a1e30fb32006d11ca6a2597df9d978
Kevin <moralesperezkevin7@gmail.com>
2024-07-01 14:09:51 -0600

    Agregando la pagina informationProduc

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Home from './Paginas/Home.jsx'
+import InformationProduct from './Paginas/InformationProduct.jsx';
 
 function App() {
   return (
---
+++ src/Paginas/InformationProduct.jsx
@@ -0,0 +1,9 @@
+import NavBar from "../Componentes/NavBar/NavBar";
+
+export default function InformationProduct(){
+    return(
+        <div className="box-container">
+            <NavBar/>
+        </div>
+    );
+}
