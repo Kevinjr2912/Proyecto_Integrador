3060d50a16e1600b8cc3017a9862e5e17aff115b
Andresito126 <creed126ax@gmail.com>
2024-07-08 19:23:56 -0600

    todo el main listo y unido

---
+++ src/Componentes/GestionProductos/Buscador.jsx
@@ -0,0 +1,16 @@
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons';
+import '../Estilos/Buscador.css';
+
+export default function Buscador({className}) {
+  return (
+    <div className={`${className}`}>
+        <div className='search-bar'>
+            <input type="text" placeholder='Ingrese el nombre del producto'/>
+            <div className="actions">
+                <FontAwesomeIcon className="search_button" icon={faMagnifyingGlass} />
+            </div>
+        </div>
+    </div>
+  );
+}
