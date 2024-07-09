ab4281acfdbb803bb9355d383f4fe7e8c375b559
Andresito126 <creed126ax@gmail.com>
2024-07-08 19:59:11 -0600

    main listo

---
+++ Servidor/src/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const customersRoutes = require('./routes/clientes');
+const productsRoutes = require('./routes/productos');
+
+const app = express();
+const port = process.env.DB_PORT || 3000;   
+
+app.use(cors());
+
+// Middleware para analizar los cuerpos de las solicitudes
+app.use(bodyParser.json());
+
+// Usar las rutas de los items
+app.use('/customers', customersRoutes);
+app.use('/products', productsRoutes);
+// Iniciar el servidor
+app.listen(port, () => {
+    console.log(`Servidor Express en ejecución en http://localhost:${port}`);
+});
