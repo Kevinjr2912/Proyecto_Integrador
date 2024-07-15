d53aed0493da3032ac7f86800d076c80153fe511
Kevin <moralesperezkevin7@gmail.com>
2024-07-15 14:59:39 -0600

    datos domicilio terminado

--- Cliente/src/Componentes/ShippingData.jsx
+++ Cliente/src/Componentes/ShippingData.jsx
@@ -20,16 +20,28 @@ export default function ShippingData() {
         setEstado(data.codigo_postal.estado);
         setMunicipio(data.codigo_postal.municipio);
         setColonias(data.codigo_postal.colonias);
-
-        console.log("Valor para calle " + calle);
-        
     }catch(err){
         console.log('Error al enviar la petición al servidor');
     }
   }
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if(document.getElementById('calle').value == "" || document.getElementById('textReferencia').value == ""){
+        Swal.fire({
+            icon: "errorr",
+            title: "Oops...",
+            text: `No pueden quedar campos vacíos`,
+        });
+    } 
+    console.log("Valor para calle" + calle);
+    console.log("Valor para numero exterior " +  numeroExterior);
+    console.log("Valor para referencia " + referencia)
+  }
+
   return (
-    <form className="containerShippingData">
+    <form className="containerShippingData" onSubmit={handleSubmit}>
       <label htmlFor="codigo_postal">Código postal</label>
       <input
         type="text"
@@ -42,7 +54,13 @@ export default function ShippingData() {
       <div className="referencias">
         <div className="box_estado">
           <label htmlFor="estado">Estado</label>
-          <input type="text" name="Estado" id="estado" placeholder="Estado" value={estado} readOnly/>
+          <input 
+          type="text" 
+          name="Estado" 
+          id="estado" 
+          placeholder="Estado" 
+          value={estado} 
+          readOnly/>
         </div>
         <div className="box_municipio">
           <label htmlFor="municipio">Municipio</label>
@@ -66,7 +84,14 @@ export default function ShippingData() {
       <div className="referencia_cercana">
         <div className="box_calle">
           <label htmlFor="calle">Calle</label>
-          <input type="text" name="Calle" id="calle" placeholder="Calle" />
+          <input 
+          type="text" 
+          name="Calle" 
+          id="calle" 
+          placeholder="Calle" 
+          value={calle}
+          onChange={(e) => setCalle(e.target.value)}
+          />
         </div>
         <div className="box_numeroExterior">
           <label htmlFor="numero_exterior">Número exterior</label>
@@ -75,6 +100,8 @@ export default function ShippingData() {
             name="NumeroExterior"
             id="numero_exterior"
             placeholder="Número exterior"
+            value={numeroExterior}
+            onChange={(e) => setNumeroExterior(e.target.value)}
           />
         </div>
       </div>
@@ -83,11 +110,13 @@ export default function ShippingData() {
         name="textReferencia"
         id="textReferencia"
         maxLength="330"
+        value={referencia}
+        onChange={(e) => setReferencia(e.target.value)}
       ></textarea>
 
       <div className="actionsData">
         <button className="btn_cancelar">Cancelar</button>
-        <button className="saveData">Guardar datos envío</button>
+        <button className="saveData" type="submit">Guardar datos envío</button>
       </div>
     </form>
   );
