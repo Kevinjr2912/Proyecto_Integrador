b16eb60dcbc8136a923790f444a96b63f21273f1
Kevin <moralesperezkevin7@gmail.com>
2024-07-20 23:23:39 -0600

    :)

--- Cliente/src/Componentes/Filtros.jsx
+++ Cliente/src/Componentes/Filtros.jsx
@@ -15,7 +15,7 @@ export default function Filtros({ onFilterChange }) {
     });
   };
 
-  React.useEffect(() => {
+  useEffect(() => {
     onFilterChange(selectedTeams);
   }, [selectedTeams, onFilterChange]);
 
