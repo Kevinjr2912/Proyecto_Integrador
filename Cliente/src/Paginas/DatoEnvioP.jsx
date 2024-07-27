9565b9f0d98eb9eada480299b34913b696a825a6
Kevin <moralesperezkevin7@gmail.com>
2024-07-27 15:45:11 -0600

    Cambios por hacer en shippingData

--- Cliente/src/Paginas/DatoEnvioP.jsx
+++ Cliente/src/Paginas/DatoEnvioP.jsx
@@ -3,42 +3,40 @@ import NavBar from "../Componentes/NavBar";
 import WhatsFlotante from "../Componentes/WhatsFlotante";
 import Footer from "../Componentes/Footer";
 import ShippingData from "../Componentes/ShippingData";
-import styles from "../Estilos/DatoEnvioP.module.css"
+import styles from "../Estilos/DatoEnvioP.module.css";
 
+export default function DatoEnvioP() {
+  const seccionesNav = [
+    {
+      id: 0,
+      nombre: "CONOCENOS",
+    },
+    {
+      id: 1,
+      nombre: "OVEROLES",
+    },
+    {
+      id: 2,
+      nombre: "CASCOS",
+    },
+    {
+      id: 3,
+      nombre: "MIS ORDENES",
+    },
+  ];
+  return (
+    <>
+      <WhatsFlotante></WhatsFlotante>
+      <NavBar seccionesNav={seccionesNav} esSeccionCliente={true} />
 
-export default function DatoEnvioP(){
-    const seccionesNav = [
-        {
-          id: 0,
-          nombre: "CONOCENOS",
-        },
-        {
-          id: 1,
-          nombre: "OVEROLES",
-        },
-        {
-          id: 2,
-          nombre: "CASCOS",
-        },
-        {
-          id: 3,
-          nombre: "MIS ORDENES",
-        },
-        
-    
-      ];
-    return(
-        <>
-        <WhatsFlotante></WhatsFlotante>
-         <NavBar seccionesNav={seccionesNav} esSeccionCliente={true} />
-        
-        <h1>DATOS DE ENVIO.</h1>
-
+      <div className={styles.containerShippingData}>
+        <div className={styles.subcontainerShippingData}>
+        <h2 className={styles.tittleShippingData}>DATOS DE ENVÍO</h2>
         <ShippingData></ShippingData>
+        </div>
+      </div>
 
-        <Footer></Footer>
-        </>
-
-    )
-
+      <Footer></Footer>
+    </>
+  );
 }
